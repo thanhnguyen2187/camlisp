d9a69b5dd721db8776a0f990bda397f8818ba073
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-04-20 02:30:25 +0700

    Change `tokenize`'s implementation to a queue
    A queue is used instead of a stack, since the future `Parser`, which is
    going to receive `token`s, would wants to traverse the parsed data
    sequentially.

--- bin/main.ml
+++ bin/main.ml
@@ -2,7 +2,7 @@ open Camlisp.Tokenizer;;
 
 while true do
     let line = read_line()
-    in Stack.iter Tokenizer.print_token (Tokenizer.tokenize line);
+    in Queue.iter Tokenizer.print_token (Tokenizer.tokenize line);
     ()
 done
 
--- lib/tokenizer.ml
+++ lib/tokenizer.ml
@@ -13,32 +13,32 @@ module Tokenizer =
             | Word(expr) ->
                 print_string ("<Word: " ^ expr ^ ">")
         let tokenize text =
-            let stack = Stack.create() in
+            let queue = Queue.create() in
             let rec f expr text =
             let n = String.length text in
             if n = 0 then
                 begin
                     if expr != ""
-                    then Stack.push (Word expr) stack;
-                    stack
+                    then Queue.push (Word expr) queue;
+                    queue
                 end
             else
                 let ch = String.sub text 0 1 in
                 let rest_text = String.sub text 1 (if n = 1 then 0 else n - 1) in
                 match ch with
                 | "(" ->
-                    Stack.push OpeningBracket stack;
+                    Queue.push OpeningBracket queue;
                     f expr rest_text
                 | ")" ->
                     if expr != ""
-                    then Stack.push (Word expr) stack;
+                    then Queue.push (Word expr) queue;
 
-                    Stack.push ClosingBracket stack;
+                    Queue.push ClosingBracket queue;
                     let new_expr = (if expr != "" then "" else expr) in
                     f new_expr rest_text
                 | " " | "\n" | "\t" ->
                     if expr != ""
-                    then Stack.push (Word expr) stack;
+                    then Queue.push (Word expr) queue;
                     f "" rest_text
                 | c ->
                     f (expr ^ c) rest_text
