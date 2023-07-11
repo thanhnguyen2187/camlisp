f268661fedd98b347143232bb4bfe33d7400a66f
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-07-11 13:46:43 +0700

    Split code to `cli.ml` and `repl.ml`

---
+++ bin/repl.ml
@@ -0,0 +1,29 @@
+open Camlisp
+
+type input_state =
+    | Prompting of string
+    | Done
+
+let rec read_eval_print env state curr =
+    match state with
+    | Prompting (characters) ->
+        print_string (characters ^ " ");
+        let line = read_line () in
+        let expr = curr ^ " " ^ line in
+        (* TODO: handle the case where prompting gets into an infinite loop when
+                 there are redundant closing brackets *)
+        if Tokenizer.is_balanced expr
+        then read_eval_print env Done expr
+        else read_eval_print env (Prompting "..") expr
+    | Done ->
+        Evaluator.eval_str env curr
+        |> List.iter
+            (fun node_string ->
+                print_string node_string;
+                print_newline ())
+
+let start () =
+    let env = Evaluator.default_env in
+    while true do
+        read_eval_print env (Prompting "=>") ""
+    done
